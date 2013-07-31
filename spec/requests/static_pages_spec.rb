7d70a0c94af3828a5b23dd4cbff4e86234cdde3b
Lex Javier <lex.javier@gmail.com>
2013-07-31 20:52:27 +0800

    Modify Home and Help content

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+
+  describe "Home page" do
+
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home' # use the Capybara function visit
+                                 # to simulate vising the URL
+      expect(page).to have_content('Sample App') # use the Capybara page variable
+    end
+  end
+
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+  end
+end
